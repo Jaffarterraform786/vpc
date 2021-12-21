91b36534f28cf7d29b4711f350fbac09554dbddd
Rex Lorenzo <rexlorenzo@gmail.com>
2021-12-21 08:54:21 -0800

    #2 Fixed InvalidElasticIpID.Malformed issue

--- natgateway.tf
+++ natgateway.tf
@@ -1,5 +1,5 @@
 resource "aws_nat_gateway" "natgateway" {
-  allocation_id = "aws_eip.ip.id"
+  allocation_id = "aws_eip.eip.id"
   subnet_id     = "aws_subnet.publicsubnet.id"
   tags = {
     name = "prod nategatway"
